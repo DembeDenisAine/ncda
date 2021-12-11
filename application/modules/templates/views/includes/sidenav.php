bd0ec2a98e38912e0e2df0fc4e60d9e0be3cf795
Ainedembe Denis <dembedenisjb@gmail.com>
2021-12-11 13:16:29 +0300

    added facility link on nav bar

--- application/modules/templates/views/includes/sidenav.php
+++ application/modules/templates/views/includes/sidenav.php
@@ -197,7 +197,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="#" class="nav-link">
+                    <a href="<?=site_url('facility-list')?>" class="nav-link">
                         <i class="far fa-circle nav-icon"></i>
                         <p>Facilities</p>
                     </a>
