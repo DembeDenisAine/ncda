7e453359df275143a5a11843b4415fb7c5c7d071
nkukehenry <henricsanyu@gmail.com>
2022-01-16 17:35:31 +0300

    Menu Clean Up

--- application/modules/templates/views/includes/sidenav.php
+++ application/modules/templates/views/includes/sidenav.php
@@ -19,25 +19,10 @@
       
       <div class="user-panel mt-3 pb-3 mb-3" style="text-align:center; line-height:0.2cm;">
 
-      <p class="brand-image img-circle " style="color:#FEFFFF; font-size: 11px; height:20px; font-weight:bold; margin-top:2px; opacity: .8;">
-
-      <?php  
-
-          echo strtoupper($userdata['names']); 
-
-
-          ?>
-         
-          
-
-      </p>
-          <hr>
-        <p style="color:#FEFFFF; font-size: 10px; height:20px; font-weight:bold; margin-top:1px;">
-       
-        </p>
-        <!-- <div class="image">
-          <p ><img src="<?php echo base_url(); ?>assets/img/user.jpg" class="img-circle elevation-2" alt="User Image" style="width:35px; height:35px;"></p>
-        </div> -->
+        <div class="image">
+          <img src="<?php echo base_url(); ?>assets/img/user.jpg" class="img-circle elevation-2" alt="User Image" style="width:50px; height:50px;">
+           <span class="text-white"><?php   echo strtoupper(truncate($userdata['names'],1230));  ?></span>
+        </div>
       
     </div>
 
@@ -80,69 +65,8 @@
                 </li>
             </ul>
         </li>
-        <li class="nav-item">
-            <a href="#" class="nav-link">
-                <i class="nav-icon fas fa-chart-pie"></i>
-                <p>
-                    Objectives
-                    <i class="right fas fa-angle-left"></i>
-                </p>
-            </a>
-            <ul class="nav nav-treeview">
-                <li class="nav-item">
-                    <a href="<?=site_url('objective-list')?>" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>List Objectives</p>
-                    </a>
-                </li>
-            </ul>
-        </li>
-        <li class="nav-item">
-            <a href="#" class="nav-link">
-                <i class="nav-icon fas fa-chart-pie"></i>
-                <p>
-                    Activities
-                    <i class="right fas fa-angle-left"></i>
-                </p>
-            </a>
-            <ul class="nav nav-treeview">
-                <li class="nav-item">
-                    <a href="<?=site_url('activity-list')?>" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>List Activities</p>
-                    </a>
-                </li>
-            </ul>
-        </li>
-        <li class="nav-item">
-            <a href="#" class="nav-link">
-                <i class="nav-icon fas fa-tree"></i>
-                <p>
-                    Reports
-                    <i class="fas fa-angle-left right"></i>
-                </p>
-            </a>
-            <ul class="nav nav-treeview">
-                <li class="nav-item">
-                    <a href="" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>Project Report</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>DHTs Report</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="" class="nav-link">
-                        <i class="far fa-circle nav-icon"></i>
-                        <p>Donation Report</p>
-                    </a>
-                </li>
-            </ul>
-        </li>
+     
+        
         <li class="nav-item">
             <a href="#" class="nav-link">
                 <i class="nav-icon fas fa-edit"></i>
@@ -167,17 +91,8 @@
             </ul>
         </li>
         <li class="nav-item">
-            <a href="<?=base_url()?>contacts" class="nav-link">
-                <i class="nav-icon far fa-calendar-alt"></i>
-                <p>
-                    Contacts
-                    <span class="badge badge-info right">2</span>
-                </p>
-            </a>
-        </li>
-        <li class="nav-item">
             <a href="#" class="nav-link">
-                <i class="nav-icon fas fa-table"></i>
+                <i class="nav-icon fa fa-map"></i>
                 <p>
                     Locations/Territory
                     <i class="fas fa-angle-left right"></i>
@@ -204,9 +119,19 @@
                 </li>
             </ul>
         </li>
+        
+        <li class="nav-item">
+            <a href="<?=base_url()?>contacts" class="nav-link">
+                <i class="nav-icon far fa-calendar-alt"></i>
+                <p>
+                    Contacts
+                    <span class="badge badge-info right">People</span>
+                </p>
+            </a>
+        </li>
         <li class="nav-item">
             <a href="#" class="nav-link">
-                <i class="nav-icon far fa-envelope"></i>
+                <i class="nav-icon fa fa-archive"></i>
                 <p>
                     Extras
                     <i class="fas fa-angle-left right"></i>
@@ -221,8 +146,36 @@
                 </li>
             </ul>
         </li>
-
-                  <!--user perm 14-->
+        <li class="nav-item">
+            <a href="#" class="nav-link">
+                <i class="nav-icon fas fa-list"></i>
+                <p>
+                    Reports
+                    <i class="fas fa-angle-left right"></i>
+                </p>
+            </a>
+            <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Project Report</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>DHTs Report</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Donation Report</p>
+                    </a>
+                </li>
+            </ul>
+        </li>
+           <!--user perm 14-->
           <?php if(in_array('35', $permissions)){ ?>
           <li class="nav-item has-treeview ">
             <a href="#" class="nav-link">
@@ -263,44 +216,7 @@
                 <?php } ?>
             </ul>
           </li>
-        
-           <!--user perm 26-->
-           <?php if(in_array('32', $permissions)){ ?>
-            <li class="nav-item has-treeview ">
-            <a href="#" class="nav-link">
-              <i class="fa fa-check-square"></i>
-              <p>
-                Forms & Fields
-                <i class="fas fa-angle-left right"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-                <li class="nav-item">
-                  <a href="<?php echo base_url()?>forms" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  Forms</a>
-                  </li>
-                  <li class="nav-item">
-                  <a href="<?php echo base_url()?>forms/fields" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  Fields</a>
-                  </li>
-            </ul>
-            </li>
-            <?php } ?>
-
-
           <?php } ?>
-         <li class="nav-item has-treeview">
-            <a href="<?php echo base_url(); ?>" class="nav-link" 
-            class="passchange nav-link dropdown-toggle" data-toggle="modal" role="button" 
-            data-target="#changepassword">
-              <i class="fa fa-lock"></i>
-              <p>
-               Change Password
-                </i>
-              </p>
-            </a>
           </li>
         </ul>
       </nav>
